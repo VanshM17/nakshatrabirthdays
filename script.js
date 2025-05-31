748024a220e43eb28b7d00889059ffcaa023a959
VanshM17 <93506555+VanshM17@users.noreply.github.com>
2025-05-31 17:20:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,114 @@
+//arrays for 2025
+const members2025 = [
+  { name: "Ayush Jha", birthday: "29-Jul", title: "President", photo: "placeholder.jpg" },
+  { name: "Kritika Choudhary", birthday: "26-Oct", title: "President", photo: "placeholder.jpg" },
+  { name: "Vatsal Shrivastava", birthday: "26-Nov", title: "Head of Design", photo: "placeholder.jpg" },
+  { name: "Paridhi Gupta", birthday: "7-Aug", title: "Vice-President", photo: "placeholder.jpg" },
+  ];
+
+//arrays for 2026
+const members2026 = [
+  { name: "Ghanshyam Jonwal", birthday: "17-Feb", title: "Member", photo: "placeholder.jpg" },
+  { name: "Meenakshi Sinha", birthday: "23-Feb", title: "Member", photo: "placeholder.jpg" },
+  { name: "Ayush Dewangan", birthday: "27-Jun", title: "Member", photo: "placeholder.jpg" },
+  { name: "Omkar Ahuja", birthday: "15-Aug", title: "Member", photo: "placeholder.jpg" },
+  { name: "Ayushman Bari", birthday: "15-Aug", title: "Member", photo: "placeholder.jpg" },
+  { name: "Divye Bajaj", birthday: "31-Aug", title: "Member", photo: "placeholder.jpg" },
+  { name: "Avantika Ambra", birthday: "13-Dec", title: "Member", photo: "placeholder.jpg" }
+];
+
+//arrays for 2027
+const members2027 = [
+  { name: "Ayush Kumar", birthday: "7-Jan", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Gautam Bajaj", birthday: "7-Jan", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Satyam Parida", birthday: "17-Jan", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Anshika Singh", birthday: "27-Feb", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Manav Jaiswal", birthday: "12-Mar", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Aviral Gaur", birthday: "24-Mar", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Aakash Jha", birthday: "25-Mar", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Harshit Bindal", birthday: "5-Apr", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Rahul", birthday: "15-Apr", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Srishti Tayal", birthday: "15-Jun", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Ashu Anand", birthday: "8-Aug", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Tarun Sharma", birthday: "7-Sep", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Harsh Swami", birthday: "11-Sep", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Dipankar Page", birthday: "21-Oct", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Yogita Jha", birthday: "5-Nov", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Nandini Nautiyal", birthday: "17-Nov", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Vansh Maheshwari", birthday: "17-Nov", title: "Execomm Member", photo: "placeholder.jpg" },
+  { name: "Rishabh Jain", birthday: "23-Dec", title: "Execomm Member", photo: "placeholder.jpg" }
+];
+
+//arrays for 2028
+const members2028 = [
+  { name: "Mayank Gupta", birthday: "7-Jan", title: "Member", photo: "placeholder.jpg" },
+  { name: "Prajal Goel", birthday: "9-Mar", title: "Member", photo: "placeholder.jpg" },
+  { name: "Tijil Gupta", birthday: "9-Mar", title: "Member", photo: "placeholder.jpg" },
+  { name: "Rachit Kathuria", birthday: "14-Apr", title: "Member", photo: "placeholder.jpg" },
+  { name: "Sneha Kumari", birthday: "16-Apr", title: "Member", photo: "placeholder.jpg" },
+  { name: "Shreem", birthday: "29-Apr", title: "Member", photo: "placeholder.jpg" },
+  { name: "Aujasvi Saxena", birthday: "2-May", title: "Member", photo: "placeholder.jpg" },
+  { name: "Vini Bansode", birthday: "31-May", title: "Member", photo: "placeholder.jpg" },
+  { name: "Bhavya Goel", birthday: "11-Jul", title: "Member", photo: "placeholder.jpg" },
+  { name: "Stuti Jain", birthday: "26-Sep", title: "Member", photo: "placeholder.jpg" },
+  { name: "Laksh Agrawal", birthday: "8-Oct", title: "Member", photo: "placeholder.jpg" },
+  { name: "Ayush Sinha", birthday: "20-Nov", title: "Member", photo: "placeholder.jpg" },
+  { name: "Aaryan Kumar", birthday: "30-Nov", title: "Member", photo: "placeholder.jpg" },
+  { name: "Gaurish Malhotra", birthday: "7-Dec", title: "Member", photo: "placeholder.jpg" },
+  { name: "Akshat Kashyap", birthday: "13-Dec", title: "Member", photo: "placeholder.jpg" }
+];
+
+// 2. Render function (ONE for all years)
+function renderMembers(year, members) {
+  const container = document.getElementById('members-' + year);
+  container.innerHTML = '';
+  members.forEach(member => {
+    container.innerHTML += `
+      <div class="member">
+        <img src="images/${member.photo}" alt="${member.name}" class="member-photo">
+        <div class="member-info">
+          <div class="member-name">${member.name}</div>
+          <div class="member-title ${member.title.toLowerCase()}">${member.title}</div>
+          <div class="member-birthday">${member.birthday}</div>
+        </div>
+      </div>
+    `;
+  });
+}
+
+// 3. Sort function (ONE for all years)
+function sortMembers(year) {
+  let members, select;
+  if (year === '2025') {
+    members = members2025.slice();
+    select = document.getElementById('sort-2025').value;
+  } else if (year === '2026') {
+    members = members2026.slice();
+    select = document.getElementById('sort-2026').value;
+  } else if (year === '2027') {
+    members = members2027.slice();
+    select = document.getElementById('sort-2027').value;
+  } else if (year === '2028') {
+    members = members2028.slice();
+    select = document.getElementById('sort-2028').value;
+  }
+
+  if (select === 'dob') {
+    members.sort((a, b) => {
+      const parse = s => {
+        let [day, mon] = s.split('-');
+        return new Date(2000, ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].indexOf(mon), parseInt(day));
+      };
+      return parse(a.birthday) - parse(b.birthday);
+    });
+  } else if (select === 'alpha') {
+    members.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  // else 'general': keep original order
+
+  renderMembers(year, members);
+}
+
 function openYear(evt, year) {
   // Hide all tab contents
   var tabcontent = document.getElementsByClassName("tab-content");
@@ -14,12 +125,17 @@ function openYear(evt, year) {
   // Show the selected tab content and set the button as active
   document.getElementById(year).style.display = "block";
   evt.currentTarget.className += " active";
+
+  // Render members for the selected year
+  sortMembers(year);
 }
+
 
 // Open the first tab by default when the page loads
 window.onload = function() {
   var firstTab = document.getElementsByClassName('tab-button')[0];
   if (firstTab) {
-    firstTab.click();
+    firstTab.click(); // This will now also render members because of the updated openYear
   }
 };
+
